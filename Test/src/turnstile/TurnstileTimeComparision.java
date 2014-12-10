903b31ac559f875f28771f17454fe228d4d9b8f5
willlowery <william.lowery@rocky.edu>
2014-12-10 12:31:47 -0700

    Addition is important to get an accurate report

--- Test/src/turnstile/TurnstileTimeComparision.java
+++ Test/src/turnstile/TurnstileTimeComparision.java
@@ -1,14 +1,18 @@
 package turnstile;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import turnstile.Turnstile.Action;
 
 public class TurnstileTimeComparision {
 
-    static final int RUN_SIZE = 10;
-    static final int NUMBER_OF_RUNS = 100;
-    static final Action NOOP = () -> {};
+    static final int RUN_SIZE = 100;
+    static final int NUMBER_OF_RUNS = 1000000;
+    static final Action NOOP = new Action() {
+
+        @Override
+        public void act() {
+     
+        }
+    };
 
     public static void main(String[] args) {
         TurnstileSwitchImp switchImp = new TurnstileSwitchImp(Turnstile.STATE.OPEN, NOOP, NOOP, NOOP, NOOP);
@@ -16,18 +20,18 @@ public class TurnstileTimeComparision {
 
         long total = 0;
         for (int i = 0; i < NUMBER_OF_RUNS; i++) {
-            total = time(tableImp, RUN_SIZE) - time(switchImp, RUN_SIZE);
+            total += (time(tableImp, RUN_SIZE) - time(switchImp, RUN_SIZE));
         }
         System.out.println(String.format("The average difference between times is %.4f ms.", total / (double) NUMBER_OF_RUNS));
     }
 
     private static long time(Turnstile turnstile, int sampleSize) {
-        LocalDateTime start = LocalDateTime.now();
+        long start = System.currentTimeMillis();
         for (int i = 0; i < sampleSize; i++) {
             turnstile.handleEvent(randomEvent());
         }
-        LocalDateTime end = LocalDateTime.now();
-        return ChronoUnit.MILLIS.between(start, end);
+        long end = System.currentTimeMillis();
+        return end - start;
     }
 
     private static Turnstile.EVENT randomEvent() {
